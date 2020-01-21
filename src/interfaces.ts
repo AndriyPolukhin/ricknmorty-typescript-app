90bb65e822f021a85160f258c903cceba5b45c26
AndriyPolukhin <andriy.polukhin@gmail.com>
2020-01-21 18:34:58 +0200

    typescript app

---
+++ src/interfaces.ts
@@ -0,0 +1,43 @@
+/**
+ * ALL THE INTERFACES
+ */
+
+export type Dispatch = React.Dispatch<IAction>;
+
+export interface IState {
+  episodes: Array<IEpisode>;
+  favourites: Array<IEpisode>;
+}
+
+export interface IAction {
+  type: string;
+  payload: any;
+}
+
+export interface IEpisode {
+  id: number;
+  url: string;
+  name: string;
+  season: number;
+  number: number;
+  airdate: string;
+  airtime: string;
+  airstamp: string;
+  runtime: number;
+  image: {
+    medium: string;
+    original: string;
+  };
+  summary: string;
+}
+
+export interface IEpisodeProps {
+  episodes: IEpisode[];
+  store: { state: IState; dispatch: Dispatch };
+  toggleFavAction: (
+    state: IState,
+    dispatch: Dispatch,
+    episode: IEpisode
+  ) => IAction;
+  favourites: IEpisode[];
+}
